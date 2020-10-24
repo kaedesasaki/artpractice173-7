c94968753ebd8547d7432881f7584ba570e961d4
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-24 23:59:49 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,20 @@
-const CANVAS_WIDTH = 600;
-const CANVAS_HEIGHT = 600;
-const BG_COLOR = [254, 249, 232];
-let kiki;
-let kikiAnim;
+var anim_kiki, anim_lala;
 
-function preload() {
-  const kikiSpritesheet = loadSpriteSheet("img/kiki.png", 32, 32, 6);
-  kikiAnim = loadAnimation(kikiSpritesheet);
-  kiki = createSprite(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 32, 32);
-  kiki.moveSpeed = 5;
+function preload(){
+  anim_kiki = loadAnimation("assets/kiki1.png","assets/kiki4.png");
+  anim_lala = loadAnimation("assets/lala1.png","assets/lala4.png");
+}
+	
+function setup(){
+  createCanvas(1000, 1000);
+  var sprite = createSprite(100, 100);
+  sprite.addAnimation("kiki", anim_kiki);
+  sprite = createSprite(200, 100); 
+  sprite.addAnimation("lala", anim_lala); 
 }
 
-function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  kiki.addAnimation("move", kikiAnim);
-  kiki.addImage("still", loadImage("img/kiki_still.png"));
-  kiki.setDefaultCollider();
-}
+function draw(){
+  background(158, 231, 250);
+  drawSprites();
 
-function update(object) {
-  if (keyDown("up") || keyDown("down") || keyDown("left") || keyDown("right")) {
-    if (keyDown("up")) {
-      object.addSpeed(2, 270);
-    }
-    if (keyDown("down")) {
-      object.addSpeed(2, 90);
-    }
-    if (keyDown("left")) {
-      object.addSpeed(2, 180);
-      object.mirrorX(-1);
-    }
-    if (keyDown("right")) {
-      object.addSpeed(2, 0);
-      object.mirrorX(1);
-    }
-  } else {
-    object.setSpeed(0);
-  }
-  drawObject(object);
-}
-
-function drawObject(object) {
-  if (object.getSpeed() > 0.0001) {
-    object.changeAnimation("move");
-  } else {
-    object.changeImage("still");
-  }
-  kiki.limitSpeed(kiki.moveSpeed);
-  drawSprite(object);
-}
-
-function draw() {
-  background(BG_COLOR);
-  update(kiki);
 }
